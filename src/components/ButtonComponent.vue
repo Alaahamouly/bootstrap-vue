ac3f521926cb9a2b0a5300b509977282a3326051
unknown <alaahamouly2014@gmail.com>
2022-12-31 17:50:53 +0200

    Appling b-button component

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="button">
+    <b-button>Default Button</b-button>
+    <b-button variant="success">Success Button</b-button>
+    <b-button variant="danger">danger Button</b-button>
+    <b-button variant="warning">warning Button</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonComponent',
+}
+</script>
+
