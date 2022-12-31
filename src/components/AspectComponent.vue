f519efff24cf19325db36fc8f07f50da0e2f4f3c
unknown <alaahamouly2014@gmail.com>
2022-12-31 18:49:20 +0200

    Apply b-aspect component

---
+++ src/components/AspectComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="aspect">
+    <h3>** b-aspect **</h3>
+
+    <b-button @click="changeAspect('2:1')" class="m-1">2:1</b-button>
+    <b-button @click="changeAspect('4:3')" class="m-1">4:3</b-button>
+    <b-button @click="changeAspect('16:9')" class="m-1">16:9</b-button>
+    <b-button @click="changeAspect('1:1')" class="m-1">1:1</b-button>
+
+    <b-aspect :aspect="aspect" style="border: 1px solid #999"
+      >My 2:1 aspect!</b-aspect
+    >
+    <b-row>
+      <b-col>
+        <b-aspect :aspect="aspect" style="border: 1px solid #999"
+          >My 2:1 aspect!</b-aspect
+        >
+      </b-col>
+      <b-col>
+        <b-aspect :aspect="aspect" style="border: 1px solid #999"
+          >My 2:1 aspect!</b-aspect
+        >
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AspectComponent",
+  data() {
+    return {
+      aspect: "2:1",
+    };
+  },
+  methods: {
+    changeAspect(aspect_value) {
+        this.aspect = aspect_value
+    }
+  }
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <b-container>
     <ButtonComponent />
     <AlertComponent />
+    <AspectComponent />
   </b-container>
   </div>
 </template>
@@ -11,12 +12,14 @@
 // @ is an alias to /src
 import ButtonComponent from '@/components/ButtonComponent.vue'
 import AlertComponent from '@/components/AlertComponent.vue'
+import AspectComponent from '@/components/AspectComponent.vue'
 
 export default {
   name: 'HomeView',
   components: {
     ButtonComponent,
-    AlertComponent
+    AlertComponent,
+    AspectComponent
   }
 }
 </script>
